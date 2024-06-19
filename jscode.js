934acca88297e4315bd364defc162ca0630fac0a
Yujiro54 <itocjoel947@gmail.com>
2024-06-19 07:56:28 +0200

    Add files via upload

---
+++ jscode.js
@@ -0,0 +1,13 @@
+function newPicture() {
+ 
+    document.getElementById("image").src="bilog1.jpg";
+     
+     
+    }
+     
+    function oldPicture() {
+     
+    document.getElementById("image").src="bilog2.jpg";
+     
+     
+    }
